dee94a7f7950c7e6af56bd995f938d9e68177e48
Caleb <57644769+alliraine@users.noreply.github.com>
2025-04-27 14:06:38 -0400

    fixed the error for real

--- http/tailwind.config.js
+++ http/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}"
+    "./app/**/*.{js,jsx,ts,tsx}",
     "./public/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
